21e1223a967ae7ee2c1f3ea0eb45a725459e5526
Emre Yıldız <ey56748@gmail.com>
2023-03-30 18:24:20 +0300

    ıos_widget

---
+++ ıos_widget/ıos_widgetUITests/_os_widgetUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  _os_widgetUITestsLaunchTests.swift
+//  ıos_widgetUITests
+//
+//  Created by Emre Yıldız on 30.03.2023.
+//
+
+import XCTest
+
+final class _os_widgetUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
