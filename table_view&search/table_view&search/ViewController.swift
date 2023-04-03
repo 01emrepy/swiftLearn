8a6a2a4ea99a6dd84c53074a8873621bca4badda
Emre Yıldız <ey56748@gmail.com>
2023-04-03 19:01:04 +0300

    table_view&search

---
+++ table_view&search/table_view&search/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  table_view&search
+//
+//  Created by Emre Yıldız on 3.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
