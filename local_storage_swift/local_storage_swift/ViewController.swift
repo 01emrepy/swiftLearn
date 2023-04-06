0f40f8e8bac10fb27ff519e5a5fb21529d5c2a5d
Emre Yıldız <ey56748@gmail.com>
2023-04-06 23:15:32 +0300

    init

---
+++ local_storage_swift/local_storage_swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  local_storage_swift
+//
+//  Created by Emre Yıldız on 6.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ local_storage_swift/local_storage_swiftUITests/local_storage_swiftUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  local_storage_swiftUITestsLaunchTests.swift
+//  local_storage_swiftUITests
+//
+//  Created by Emre Yıldız on 6.04.2023.
+//
+
+import XCTest
+
+final class local_storage_swiftUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
